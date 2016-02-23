5f8a54693a8fe704429ba4fe2b1f943bb080a576
Amber Brown (HawkOwl) <hawkowl@atleastfornow.net>
2016-02-23 11:45:10 +0800

    setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='rproxy',
+    description='A super simple reverse proxy.',
+    long_description=open("README.rst").read(),
+    author='Amber Brown',
+    author_email='hawkowl@atleastfornow.net',
+    packages=['rproxy', 'twisted.plugins'],
+    package_dir={"": "src"},
+    install_requires=[
+        'twisted >= 15.5.0',
+        'pyopenssl',
+        'txsni',
+        'incremental',
+    ],
+    zip_safe=False,
+    setup_requires=["incremental"],
+    use_incremental=True,
+)
